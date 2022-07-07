cfde853c6a4b6b1e0a1d71bc54e9abf32aced604
wwbel <ww.bel@ya.ru>
2022-07-07 19:56:48 +0300

    setup

--- config.tf
+++ config.tf
@@ -17,9 +17,42 @@ provider "yandex" {
   folder_id = "b1g1ea8du0rjbjnjl075"
   zone      = "ru-central1-b"
 }
-
+////////////////////////////////////////////   VM-1   ///////////////////////////////////////
 resource "yandex_compute_instance" "vm-1" {
+
   name = "devops-edu-15-1"
+
+  allow_stopping_for_update = true
+
+  metadata = {
+    user-data = "#cloud-config\nusers:\n  - name: wwbel\n    groups: sudo\n    shell: /bin/bash\n    sudo: ['ALL=(ALL) NOPASSWD:ALL']\n    ssh-authorized-keys:\n      - ${file("/Users/wwbel/.ssh/id_rsa.pub")}"
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd8qps171vp141hl7g9l" // Ubuntu 20.04
+    }
+  }
+  network_interface {
+    subnet_id = "e2lcrt85pcpnboln5af9"
+    nat = true
+  }
+  resources {
+    cores  = 4
+    memory = 4
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+////////////////////////////////////////////   VM-2   ///////////////////////////////////////
+resource "yandex_compute_instance" "vm-2" {
+
+  name = "devops-edu-15-2"
 
   allow_stopping_for_update = true
 
@@ -44,6 +77,11 @@ resource "yandex_compute_instance" "vm-1" {
   }
 }
 
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
 }
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
